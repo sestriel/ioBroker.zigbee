b8b89df0646122e80cb9868275e5725302f338b8
sestriel <felix.tzschucke@gmail.com>
2023-12-18 19:43:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -559,7 +559,7 @@ class Zigbee extends utils.Adapter {
     async processConverters(converters, devId, model, mappedModel, message, meta) {
         
         for (const converter of converters) {
-            console.log.debug(`DEBUG HERE:`, converter, model);
+            this.log.debug(`DEBUG HERE:`, converter, model);
             const publish = (payload) => {
                 this.log.debug(`Publish ${safeJsonStringify(payload)} to ${safeJsonStringify(devId)}`);
                 if (payload) {
